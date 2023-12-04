972caeab6e56448447523cb2fcf22652eaa72c91
AristoJoy <discovertheheartworld@gmail.com>
2023-12-04 23:17:15 +0800

    基础任务

--- src/kernel/task.c
+++ src/kernel/task.c
@@ -17,6 +17,7 @@ extern void task_switch(task_t *next);
 #define NR_TASK 64
 static task_t *task_table[NR_TASK];
 static list_t block_list;
+static task_t *idle_task;
 
 // 从task_table里获得一个空闲的任务
 static task_t *get_free_task()
@@ -63,6 +64,12 @@ static task_t *task_search(task_state_t state)
             task = ptr;
         }
     }
+
+    if (task == NULL && state == TASK_READY)
+    {
+        task = idle_task;
+    }
+    
 
     return task;
 }
@@ -175,43 +182,15 @@ static void task_setup()
     memset(task_table, 0, sizeof(task_table));
 }
 
-u32 _ofp thread_a()
-{
 
-    set_interrupt_state(true);
-    while (true)
-    {
-        printk("A");
-        test();
-    }
-}
-
-u32 _ofp thread_b()
-{
-    set_interrupt_state(true);
-    while (true)
-    {
-        printk("B");
-        test();
-    }
-}
-
-u32 _ofp thread_c()
-{
-    set_interrupt_state(true);
-    while (true)
-    {
-        printk("C");
-        test();
-    }
-}
+extern void idle_thread();
+extern void init_thread();
 
 void task_init()
 {
     list_init(&block_list);
 
     task_setup();
-    task_create(thread_a, "a", 5, KERNEL_USER);
-    task_create(thread_b, "b", 5, KERNEL_USER);
-    // task_create(thread_c, "c", 5, KERNEL_USER);
+    idle_task = task_create(idle_thread, "idle", 1, KERNEL_USER);
+    task_create(init_thread, "init", 5, NORMAL_USER);
 }
